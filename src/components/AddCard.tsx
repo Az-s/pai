266de60ed3cc3f4aef34c1a221ddefbffa5b4979
Az-s <azim.sa@yahoo.com>
2023-09-30 05:04:28 +0600

    added caddCard component an some styles

--- src/components/AddCard.tsx
+++ src/components/AddCard.tsx
@@ -1,8 +1,91 @@
+import { useNavigate } from "react-router-dom";
+import { TbArrowBackUp } from "react-icons/tb";
+import Visa from "../assets/visa-10.svg";
+import MC from "../assets/mastercard-2.svg";
 
 const AddCard = () => {
+  const navigate = useNavigate();
   return (
-    <div>AddCard</div>
-  )
-}
+    <>
+      <div className="w-full flex flex-col justify-between items-center p-4 rounded-2xl  bg-white">
+        <div className="w-full flex justify-center items-center">
+          <TbArrowBackUp
+            onClick={() => navigate(-1)}
+            className="cursor-pointer mr-auto"
+          />
+          <span className="text-center">Онлайн оплата</span>
+        </div>
+        <div className="w-full flex justify-between items-center">
+          <span>KG</span>
+          <span>RUS</span>
+        </div>
+      </div>
+      <h4 className="font-[400] text-[14px] text-[#a4a4a4] m-4">Заказ №237</h4>
+      <div className="w-full h-[270px] rounded-2xl  bg-white mx-4 p-3 ">
+        <div className="flex gap-2 p-4 justify-end">
+          <img width={25} height={25} src={Visa} alt="Visa" />
+          <img width={25} height={25} src={MC} alt="MasterCard" />
+        </div>
+        <form >
+          <div className="mb-4">
+            <input
+              className="mt-1 p-2 w-full border-b border-t-0 border-r-0 border-l-0 rounded-none focus:outline-none"
+              type="text"
+              placeholder="Номер карты"
+            />  
+          </div>
+          <div className="mb-4 flex justify-between">
+            <div className="w-full pr-2">
+              <input
+                className="mt-1 p-2 w-full border-b border-t-0 border-r-0 border-l-0 rounded-none focus:outline-none"
+                type="text"
+                placeholder="MM / YY"
+              />
+            </div>
+            <div className="w-full pl-2">
+              <input
+                className="mt-1 p-2 w-full border-b border-t-0 border-r-0 border-l-0 rounded-none focus:outline-none"
+                type="text"
+                placeholder="CVC"
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <input
+              className="mt-1 p-2 w-full border-b border-t-0 border-r-0 border-l-0 rounded-none focus:outline-none"
+              type="text"
+              placeholder="Имя владельца"
+            />
+          </div>
+        </form>
+      </div>
+      <div className="w-full p-4 bg-transparent">
+        <form className="">
+          <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500 ">
+            <input
+              type="text"
+              placeholder="Номер телефона"
+              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+            />
+          </div>
+
+          <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
+            <input
+              type="text"
+              placeholder="Email"
+              className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+            />
+          </div>
+        </form>
+        <p className="font-[500] text-[8px] text-[#a4a4a4] text-center">
+          Продолжая оплату, Вы соглашаетесь{" "}
+          <span className="text-[#4362e4]">
+            на сбор и обработку персональных данных
+          </span>
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default AddCard;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import { GoQuestion } from "react-icons/go";
+import Logo from '../assets/Logo.svg';
+
 const Navbar = () => {
   return (
     <div className="w-full flex h-[64px] items-center rounded-b-2xl bg-[#FFFFFF] px-[16px]">
       <div className="flex-grow text-center">
         <Link to="/">
-          <img src="path to logo" alt="Pai" />
+          <img src={Logo} alt="Pai" />
         </Link>
       </div>
       <div className="ml-auto flex items-center cursor-pointer">
