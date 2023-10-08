615c8eadf6b6b256429b0eedc2944e334ff7e5e7
Az-s <azim.sa@yahoo.com>
2023-10-09 02:01:50 +0600

    test

--- src/components/AddCardButton.tsx
+++ src/components/AddCardButton.tsx
@@ -22,7 +22,7 @@ const AddCardButton = () => {
 
 
   return (
-    <div className="rounded-t-2xl bg-[#fff] pt-[16px] pb-[32px] px-[16px] fixed bottom-0 w-full">
+    <div className="rounded-t-2xl bg-[#fff] pt-[16px] pb-[32px] px-[16px]  bottom-0 w-full">
       <button
         className="bg-purple rounded-xl w-full py-[12px] font-[600] text-[16px] px-3 text-white"
         type="button"
