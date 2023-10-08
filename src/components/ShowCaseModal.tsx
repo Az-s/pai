686dfca3260379d3e0697b7fb2e0b39c88f6e6b5
Az-s <azim.sa@yahoo.com>
2023-10-09 02:39:12 +0600

    complete modal

--- src/components/ShowCaseModal.tsx
+++ src/components/ShowCaseModal.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLottie } from "lottie-react";
-import CheckMark from '../assets/DIU_PKA0369 1.svg';
-import Attention from '../assets/Vector.svg'; 
-import animation from '../assets/animation_lnhuat53.json'
+import CheckMark from "../assets/DIU_PKA0369 1.svg";
+import Attention from "../assets/Vector.svg";
+import animation from "../assets/animation_lnhuat53.json";
+// import check from '../assets/animation_lnhx3mga.json';
 
 interface ModalProps {
   stage: number;
@@ -11,7 +12,11 @@ interface ModalProps {
 }
 
 const stages = [
-  { loader: true, heading: "Оплата проходит", buttonText: "Подождите чуть-чуть" },
+  {
+    loader: true,
+    heading: "Оплата проходит",
+    buttonText: "Подождите чуть-чуть",
+  },
   { image: CheckMark, heading: "Оплачено", buttonText: "Закрыть" },
   {
     image: Attention,
@@ -22,13 +27,13 @@ const stages = [
   {
     image: Attention,
     heading: "Ошибка перевода",
-    paragraph: "Возможно ваш банк испытывает проблемы. Попробуйте еще раз оплатить или прикрепить другую карту",
+    paragraph:
+      "Возможно ваш банк испытывает проблемы. Попробуйте еще раз оплатить или прикрепить другую карту",
     buttonText: "Попробовать снова",
   },
 ];
 
 const ShowCaseModal: React.FC<ModalProps> = ({ stage, nextStage }) => {
-    const [loading, setLoading] = useState(stages[stage].loader);
   const navigate = useNavigate();
   const { loader, image, heading, paragraph, buttonText } = stages[stage];
   const defaultOptions = {
@@ -41,25 +46,30 @@ const ShowCaseModal: React.FC<ModalProps> = ({ stage, nextStage }) => {
   };
   const { View: LottieAnimation } = useLottie(defaultOptions);
 
+//   const defaultOptionsTwo = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: check,
+//     rendererSettings: {
+//       preserveAspectRatio: "xMidYMid slice",
+//     },
+//   };
+//   const { View: LottieAnimationTwo } = useLottie(defaultOptionsTwo);
 
   useEffect(() => {
     if (loader) {
       const timer = setTimeout(() => {
-        setLoading(false);
         nextStage();
-      }, 2000); // Загрузка будет длиться 2 секунды
+      }, 3000); // Загрузка будет длиться 2 секунды
 
       return () => clearTimeout(timer); // Очистка таймера при размонтировании компонента
     }
   }, [loader, nextStage]);
 
-  if (loading) {
-    return <div>{LottieAnimation}</div>; // Отображение загрузчика
-  }
-
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-[#000] bg-opacity-50 py-[32px] px-[16px]">
       <div className="bg-white p-4 rounded-2xl flex flex-col items-center justify-center w-full">
+        {loader && <div>{LottieAnimation}</div>}
         {image && <img src={image} alt="" />}
         <h2 className="font-bold text-[20px] text-center">{heading}</h2>
         {paragraph && (
@@ -68,7 +78,9 @@ const ShowCaseModal: React.FC<ModalProps> = ({ stage, nextStage }) => {
           </p>
         )}
         <button
-          onClick={stage === stages.length - 1 ? () => navigate("/") : nextStage}
+          onClick={
+            stage === stages.length - 1 ? () => navigate("/") : nextStage
+          }
           disabled={loader}
           className="bg-purple rounded-xl w-full py-[10px] font-[600] text-[16px] px-[10px] text-white my-[24px]"
         >
